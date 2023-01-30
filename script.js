d5d5fbaae414fbb79d73bfc6b1c554bc8ac726aa
Gabriel Silva <gabriel.silva2f3d@gmail.com>
2023-01-30 13:01:04 -0300

    footer

--- script.js
+++ script.js
@@ -5,7 +5,32 @@ $(document).ready(function(){
         } else {
             $('.navbar').removeClass("sticky")
         }
+        if (this.scrollY > 20) {
+            $('.scroll-up-btn').addClass("show")
+        }else{
+            $('.scroll-up-btn').removeClass("show")
+        }
     })
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+
+    // typing animation script
+    var typed = new Typed(".typing",{
+        strings: ["Software Developer"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop: true
+    });
+    var typed = new Typed(".typing-2",{
+        strings: ["Software Developer"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop: true
+    });
+
 
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass('active');
